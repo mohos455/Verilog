b07746d9f3e53d840d250120d563fefba576a5bc
Mohamed Hesham <106884579+mohos455@users.noreply.github.com>
2023-04-18 06:30:38 +0200

    Update asyn_up_counter.v

--- asyn_up_counter_Nbit/asyn_up_counter.v
+++ asyn_up_counter_Nbit/asyn_up_counter.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/18/2023 05:49:29 AM
-// Design Name: 
-// Module Name: asyn_up_counter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module asyn_up_counter(
